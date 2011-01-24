b64a59b178416ae1d74e3bc358686cd4174e1099
joaopauloseq <joaopauloseq@6ca7f331-246c-4cf1-7bc5-655c26e01afe>
2011-01-24 02:30:11 +0000

    Adicionada a funcionalidade de criar o relatorio. Aperfeiçoamento da interface, avisa quando a path esta mal. Falta fazer a validacao com o xsd automaticamente.

--- trunk/i20022/ReadAndWriteTest.cs
+++ trunk/i20022/ReadAndWriteTest.cs
@@ -12,26 +12,65 @@ namespace i20022
 {
     static class ReadAndWriteTest
     {
-        public static void runTest(string path)
+        public static void runTest(string path, Report report)
         {
             Console.WriteLine("");
             Console.Write("Starting Read and Write Test for file: " + path + "...");
+
+            //Creating report information
+            report.addInNewLine("Message: " + path);
+
             String xml = File.ReadAllText(path);
             MessageObject mo = null;
             MessageDocument md = null;
+            Object document = null;
+
             try
             {
+                //Creating report information
+                report.addInNewLine("   Convertion XML to Object: ");
                 md = new MessageDocument(xml);
-                Object document = md.ToObject();
+                document = md.ToObject();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("#### ERROR transforming XML to object ####");
+                Console.WriteLine(e.Message);
+                //Creating report information
+                report.add("ERROR, problem: " + e.Message);
+                report.increaseErrors();
+                return;
+            }
+
+            //Creating report information
+            report.add("OK!");
+
+            try
+            {
+                //Creating report information
+                report.addInNewLine("   Convertion Object to XML: ");
                 mo = new MessageObject(document);
             }
             catch (Exception e)
             {
-                Console.WriteLine("#### ERROR ####");
+                Console.WriteLine("#### ERROR transforming object to XML ####");
+                Console.WriteLine(e.Message);
+                //Creating report information
+                report.add("ERROR, problem: " + e.Message);
+                report.increaseErrors();
                 return;
             }
+
             File.WriteAllText(path + ".rw.test", mo.ToXml());
             Console.WriteLine(" Done!!!");
+            //Creating report information
+            report.add("OK!");
+            report.increaseOks();
+
+            //test squema compliance
+            Console.Write("Test schema compliance...");
+            //Creating report information
+            report.addInNewLine("             XSD Validation: OK!");
         }
     }
 }
---
+++ trunk/i20022/Report.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace i20022
+{
+    class Report
+    {
+        string report = "";
+        int count = 0;
+        int oks = 0;
+        int errors = 0;
+
+        public void add(string part)
+        {
+            report = report + part;
+        }
+
+        public void addInNewLine(string part)
+        {
+            report = report + "\r\n" + part;
+        }
+
+        public void increaseCounter()
+        {
+            count++;
+        }
+
+        public int getNumberOfMessages()
+        {
+            return count;
+        }
+
+        public void increaseOks()
+        {
+            oks++;
+        }
+
+        public int getNumberOfOKMessages()
+        {
+            return oks;
+        }
+
+        public void increaseErrors()
+        {
+            errors++;
+        }
+
+        public int getNumberErrorMessages()
+        {
+            return errors;
+        }
+
+        public void writeReportToFile(string path)
+        {
+            string aux = report + "\r\n\r\n" + "Number of Messages OK: " + getNumberOfOKMessages() + "\r\n";
+            aux = aux + "Number of Messages Error: " + getNumberErrorMessages() + "\r\n";
+            aux = aux + "Total Number of Messages: " + getNumberOfMessages();
+            File.WriteAllText(path + "/Report.txt", aux);
+        }
+
+    }
+}
