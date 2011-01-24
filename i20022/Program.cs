425a840c85cb5b5492748c360efa62a129ebda2c
joaopauloseq <joaopauloseq@6ca7f331-246c-4cf1-7bc5-655c26e01afe>
2011-01-24 02:30:11 +0000

    Adicionada a funcionalidade de criar o relatorio. Aperfeiçoamento da interface, avisa quando a path esta mal. Falta fazer a validacao com o xsd automaticamente.

--- i20022/Program.cs
+++ i20022/Program.cs
@@ -8,7 +8,8 @@ namespace i20022
 {
     class Program
     {
-        private static void recursiveDirectorieSearchAndTestRun(string path)
+
+        private static void recursiveDirectorieSearchAndTestRun(string path, Report report)
         {
             //Get directory info
             string[] directories = Directory.GetDirectories(path);
@@ -18,29 +19,62 @@ namespace i20022
             if (files.GetLength(0) > 0)
                 foreach (string f in files)
                 {
-                    ReadAndWriteTest.runTest(f);
+                    report.addInNewLine("");
+                    report.increaseCounter();
+                    ReadAndWriteTest.runTest(f, report);
                 }
 
             //handling found directories
             if (directories.GetLength(0) > 0)
                 foreach (string d in directories)
-                    recursiveDirectorieSearchAndTestRun(d);
+                    recursiveDirectorieSearchAndTestRun(d, report);
         }
 
         static void Main(string[] args)
         {
+
             //getting path to search files
             Console.WriteLine("************** i20022 framework test tool *****************");
             Console.WriteLine("Your current directory is: " + Directory.GetCurrentDirectory());
-            Console.WriteLine("Please, enter the path to search for files. Do not finish with /");
-            string path = Console.ReadLine();
+            Console.WriteLine("Please, type the path to search for files or press enter to exit. Do not finish with /");
+            string path = null;
+            while (true)
+            {
+                path = Console.ReadLine();
+                if (path.Equals("")) return;
+                //testing path 
+                try
+                {
+                    string[] directories = Directory.GetDirectories(path);
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Wrong path, please type path again or press enter to exit.");
+                }
+            }
+
+            //creating new Report instance
+            Report report = new Report();
+            report.add("Teste and Validation of Framework i20022 (http://i20022.com/)");
+            report.addInNewLine("Testbed: " + path);
+            report.addInNewLine("");
 
             //Applying directory search and test run
-            recursiveDirectorieSearchAndTestRun(path);
+            recursiveDirectorieSearchAndTestRun(path, report);
+
+            //Creating Report
+            Console.WriteLine("");
+            Console.Write("Creating Report...");
+            report.writeReportToFile(path);
+            Console.WriteLine(" Done!!!");
 
             //Presenting final message
             Console.WriteLine("");
-            Console.Write("Test is finished, please hit enter to end.");
+            Console.WriteLine("Number of Messages OK: " + report.getNumberOfOKMessages());
+            Console.WriteLine("Number of Messages Error: " + report.getNumberErrorMessages());
+            Console.WriteLine("Total Number of Messages: " + report.getNumberOfMessages());
+            Console.Write("Test is finished, please press enter to exit.");
             Console.ReadLine();
         }
     }
